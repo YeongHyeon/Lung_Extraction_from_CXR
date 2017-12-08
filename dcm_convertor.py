3d9ec51db5ad25edba9ab9ebbf60d6ab6757c1e3
YeongHyeon <young200405@gmail.com>
2017-12-09 02:06:47 +0900

    20171209

--- dcm_convertor.py
+++ dcm_convertor.py
@@ -18,34 +18,30 @@ if(util.check_path(usr_path)):
         print("Convert: "+str(fi))
 
         tmp_name = fi.split("/")
-        tmp_sub = tmp_name[len(tmp_name)-2]
-        tmp_file = tmp_name[len(tmp_name)-2]+"_"+tmp_name[len(tmp_name)-1].split(".")[0]
+        tmp_file = tmp_name[len(tmp_name)-1].split(".")[0]
 
-        dir_sp = usr_path.split("/")
+        dir_sp = fi.split("/")
         main_dir = ""
         for ds in dir_sp[:len(dir_sp)-1]:
             main_dir += ds
             main_dir += "/"
-        main_dir += dir_sp[len(dir_sp)-1]+"_bmp"
-
-        if(not(util.check_path(path=main_dir))):
-            util.make_path(path=main_dir)
-        if(not(util.check_path(path=main_dir+"/"+tmp_sub))):
-            util.make_path(path=main_dir+"/"+tmp_sub)
 
         dicom_data = dicom.read_file(fi)
-        dicom_numpy = dicom_data.pixel_array
+        try:
+            dicom_numpy = dicom_data.pixel_array
+        except:
+            print("TypeError: No pixel data found in this dataset.")
+        else:
+            sumx = np.sum(dicom_numpy) / (dicom_numpy.shape[0]*dicom_numpy.shape[1])
+            dicom_normal = (dicom_numpy / sumx) * 127
 
-        sumx = np.sum(dicom_numpy) / (dicom_numpy.shape[0]*dicom_numpy.shape[1])
-        dicom_normal = (dicom_numpy / sumx) * 127
-
-        area1 = np.mean(dicom_normal[:int(dicom_numpy.shape[0]/4), :int(dicom_numpy.shape[1]/4)])
-        area2 = np.mean(dicom_normal[int(dicom_numpy.shape[0]/4*3):, :int(dicom_numpy.shape[1]/4)])
-        area3 = np.mean(dicom_normal[:int(dicom_numpy.shape[0]/4), int(dicom_numpy.shape[1]/4*3):])
-        area4 = np.mean(dicom_normal[int(dicom_numpy.shape[0]/4*3):, int(dicom_numpy.shape[1]/4*3):])
-
-        threshold = np.mean([area1, area2, area3, area4])
-        if(threshold > 127):
-            dicom_normal = 255 - dicom_normal
-
-        cvf.save_image(path=main_dir+"/"+tmp_sub+"/", filename=tmp_file+".bmp", image=dicom_normal)
+            # area1 = np.mean(dicom_normal[:int(dicom_numpy.shape[0]/4), :int(dicom_numpy.shape[1]/4)])
+            # area2 = np.mean(dicom_normal[int(dicom_numpy.shape[0]/4*3):, :int(dicom_numpy.shape[1]/4)])
+            # area3 = np.mean(dicom_normal[:int(dicom_numpy.shape[0]/4), int(dicom_numpy.shape[1]/4*3):])
+            # area4 = np.mean(dicom_normal[int(dicom_numpy.shape[0]/4*3):, int(dicom_numpy.shape[1]/4*3):])
+            #
+            # threshold = np.mean([area1, area2, area3, area4])
+            # if(threshold > 127):
+            #     dicom_normal = 255 - dicom_normal
+            # print(main_dir)
+            cvf.save_image(path=main_dir, filename=tmp_file+".bmp", image=dicom_normal)
